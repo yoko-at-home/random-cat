d62ed32623cec96e2980ddc685780640a5268ee2
yoko-at-home <tw107sf.ham@icloud.com>
2023-08-21 23:17:36 +0900

    サーバーサイドで実行する処理

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
-const IndexPage: NextPage = () => {
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    fetchImage().then((newImage) => {
-      setImageUrl(newImage.url);
-      setLoading(false);
-    });
-  }, []);
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
+// ページコンポーネント関数にpropsを受け取る引数を追加する
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url);
+  //     setLoading(false);
+  //   });
+  // }, []);
   const handleClick = async () => {
     setLoading(true);
     const newImage = await fetchImage();
@@ -23,6 +28,16 @@ const IndexPage: NextPage = () => {
   );
 };
 export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
 type Image = {
   url: string;
 };
